6dc022ec9e6ebc31ec4d114ff6c33ef1d308f26f
Andrew Sutherland <asutherland@asutherland.org>
2019-07-31 18:38:07 -0400

    progress

--- clang-plugin/MozsearchIndexer.cpp
+++ clang-plugin/MozsearchIndexer.cpp
@@ -1453,6 +1453,11 @@ public:
       return true;
     }
 
+    QualType qtype = QualType();
+    if (ValueDecl *D2 = dyn_cast<ValueDecl>(D)) {
+      qtype = D2->getType();
+    }
+
     SourceRange CommentRange = getCommentRange(D);
     PeekRange = combineRanges(PeekRange, CommentRange);
     PeekRange = validateRange(Loc, PeekRange);
@@ -1497,7 +1502,7 @@ public:
     }
 
     visitIdentifier(Kind, PrettyKind, getQualifiedName(D), SourceRange(Loc), Symbols,
-                    QualType(),
+                    qtype,
                     getContext(D), Flags, PeekRange, NestingRange);
 
     return true;
