b67917cb9238225ec6c7e38a1d9e4e02ccd78376
Andrew Sutherland <asutherland@asutherland.org>
2020-03-01 11:41:59 -0500

    Test files so that the clang indexer processes big_header.h multiple times.

---
+++ tests/tests/files/include_big_header_2.cpp
@@ -0,0 +1,8 @@
+// This file is one of several that exists so that the indexer's file locking
+// and multiple pass integration logic gets tested.
+
+#include "big_header.h"
+
+void foo_2() {
+  //
+}
